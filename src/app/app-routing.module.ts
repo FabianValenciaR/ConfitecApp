7e49652a5e7f6bb0e071e6a1f9832a41adf48c25
Fabian Valencia <fabianmvr@gmail.com>
2020-01-13 11:57:06 -0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then(m => m.HomePageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then(m => m.AuthPageModule)
+  },
+  {
+    path: "indicators",
+    loadChildren: () =>
+      import("./indicators/indicators.module").then(m => m.IndicatorsPageModule),
+      canLoad: [AuthGuard]
+  }
 ];
 
 @NgModule({
@@ -12,4 +28,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
