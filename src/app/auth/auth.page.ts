0a65d36f23825d6ce983e8a53ffcf473b4ce0299
Fabian Valencia <fabianmvr@gmail.com>
2020-01-14 17:30:41 -0500

    Firebase connected

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "./auth.service";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { AuthService, AuthResponseData } from "./auth.service";
 import { Router } from "@angular/router";
 import { NgForm } from "@angular/forms";
+import { LoadingController, AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-auth",
@@ -9,22 +10,90 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./auth.page.scss"]
 })
 export class AuthPage implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  @ViewChild("f", null) form: NgForm;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {}
 
-  onLogin() {
-    this.authService.login();
-    this.router.navigateByUrl("/home");
-  }
-
+  /**
+   * Validate the from and send a specific request
+   *
+   * @param {NgForm} form
+   * @returns
+   * @memberof AuthPage
+   */
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
     }
     const user = form.value.user;
     const pass = form.value.password;
+    this.onLogin(user, pass);
+  }
 
-    console.log(user, pass);
+  /**
+   * Sends the login request to the service and receive the response
+   *
+   * @param {string} email
+   * @param {string} password
+   * @memberof AuthPage
+   */
+  onLogin(email: string, password: string) {
+    this.loadingCtrl
+      .create({
+        keyboardClose: true,
+        message: "Iniciando Sesión..."
+      })
+      .then(loadingEl => {
+        loadingEl.present();
+        this.authService.login(email, password).subscribe(
+          response => {
+            this.form.reset();
+            this.router.navigateByUrl("/home");
+            loadingEl.dismiss();
+          },
+          error => {
+            this.form.reset();
+            loadingEl.dismiss();
+
+            const code = error.error.error.message;
+            let message =
+              "Ocurrió un problema al iniciar sesión, por favor intente nuevamente.";
+            if (code == "EMAIL_NOT_FOUND") {
+              message = "El correo electrónico ingresado no existe.";
+            } else if (code == "INVALID_PASSWORD") {
+              message = "La contraseña ingresada no es válida.";
+            } else if (code == "USER_DISABLED") {
+              message = "La cuenta ha sido desabilitada por el administrador.";
+            }
+            this.showAlert(message);
+          }
+        );
+      });
+  }
+
+  /**
+   * Shows an alert given an specific message
+   *
+   * @private
+   * @param {string} message
+   * @memberof AuthPage
+   */
+  private showAlert(message: string) {
+    this.alertCtrl
+      .create({
+        header: "No se pudo iniciar sesión.",
+        message: message,
+        buttons: ["Aceptar"]
+      })
+      .then(alertEl => {
+        alertEl.present();
+      });
   }
 }
