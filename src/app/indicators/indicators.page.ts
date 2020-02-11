088f2fbd0573de0b10f558d599736721364fa212
Fabian Valencia <fabianmvr@gmail.com>
2020-02-11 16:42:01 -0500

    Sending user code to BE

--- src/app/indicators/indicators.page.ts
+++ src/app/indicators/indicators.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { IndicatorsService } from "../services/indicators/indicators.service";
+import { TotalSales } from "../models/TotalSales";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
   selector: "app-indicators",
@@ -7,37 +9,91 @@ import { IndicatorsService } from "../services/indicators/indicators.service";
   styleUrls: ["./indicators.page.scss"]
 })
 export class IndicatorsPage implements OnInit {
-  constructor(private indicatorSvc: IndicatorsService) {}
+  constructor(
+    private indicatorSvc: IndicatorsService,
+    private authService: AuthService
+  ) {}
   maxDate: Date = new Date();
+  filterDate: string = new Date().toISOString();
+  totalSales: TotalSales = new TotalSales();
+  visitasEfectivas: number = 0;
+  visitasTotales: number = 0;
+  userCode: string = "";
 
   ngOnInit() {}
 
-  ionViewWillEnter() {
-    this.retrieveUsers();
-    this.retrieveSales();
-    this.retrieveTotalVisits();
-    this.retrieveDrop();
+  /**
+   *Executes the getUserData function to refresh the information according to the selected date
+   *
+   * @memberof IndicatorsPage
+   */
+  onDateChange() {
+    this.getUserData();
   }
 
+  /**
+   *Executes the getUserData function when enters to the view
+   *
+   * @memberof IndicatorsPage
+   */
+  ionViewWillEnter() {
+    this.authService.onMemoryCode.then(code => {
+      this.userCode = `"${code["value"]}"`;
+      this.getUserData();
+    });
+  }
+
+  /**
+   *Get all the data for a specific user
+   *
+   * @memberof IndicatorsPage
+   */
+  getUserData() {
+    this.retrieveUsers();
+    this.retrieveSales(this.userCode);
+    this.retrieveTotalVisits(this.userCode);
+    this.retrieveDrop(this.userCode);
+  }
+
+  /**
+   *Get all the active users
+   *
+   * @memberof IndicatorsPage
+   */
   retrieveUsers() {
     this.indicatorSvc.getUsers().subscribe(response => {});
   }
 
-  retrieveSales() {
-    this.indicatorSvc.getSales("1102126735", "0").subscribe(response => {
-      console.log(response);
+  /**
+   *Get the current sales given a specific userCode and Date
+   *
+   * @memberof IndicatorsPage
+   */
+  retrieveSales(userCode: string) {
+    this.indicatorSvc.getSales(this.userCode, "0").subscribe(response => {
+      this.totalSales = response[0];
     });
   }
 
-  retrieveDrop() {
-    this.indicatorSvc.getCurrentDrop("1102126735", "0").subscribe(response => {
-      console.log(response);
+  /**
+   *Get the current drop given a specific userCode and Date
+   *
+   * @memberof IndicatorsPage
+   */
+  retrieveDrop(iuserCode: string) {
+    this.indicatorSvc.getCurrentDrop(this.userCode, "0").subscribe(response => {
+      this.visitasEfectivas = response[0]["visitasEfectivas"];
     });
   }
 
-  retrieveTotalVisits() {
-    this.indicatorSvc.getTotalVisits("1102126735", "0").subscribe(response => {
-      console.log(response);
+  /**
+   *Get the total visits given a specific userCode and date
+   *
+   * @memberof IndicatorsPage
+   */
+  retrieveTotalVisits(userCode: string) {
+    this.indicatorSvc.getTotalVisits(this.userCode, "0").subscribe(response => {
+      this.visitasTotales = response[0]["visitasTotales"];
     });
   }
 }
