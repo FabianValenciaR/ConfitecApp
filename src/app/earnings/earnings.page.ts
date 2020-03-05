dfd0a95d93dc519633983aa038a4266f51f85870
Fabian Valencia <fabianmvr@gmail.com>
2020-03-05 13:55:06 -0500

    Earnings section created and guard added

---
+++ src/app/earnings/earnings.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-earnings",
+  templateUrl: "./earnings.page.html",
+  styleUrls: ["./earnings.page.scss"]
+})
+export class EarningsPage implements OnInit {
+  // Tab selected by default
+  selectedTab: string = "daily";
+
+  constructor() {}
+
+  selectTab(tab: string) {
+    // Assign the selected tab to the variable
+    this.selectedTab = tab;
+
+    //Remove all the active classes
+    document.getElementById("daily").classList.remove("active-tab");
+    document.getElementById("weekly").classList.remove("active-tab");
+    document.getElementById("monthly").classList.remove("active-tab");
+
+    //Add the active class to the selected tab
+    document.getElementById(tab).classList.add("active-tab");
+  }
+
+  ngOnInit() {}
+}
