0cbb9128dfa8be931dc7671ad255cbde003cc164
Fabian Valencia <fabianmvr@gmail.com>
2020-03-13 13:04:50 -0500

    Effective visit indicator done

--- src/app/components/effective-visit/effective-visit.component.ts
+++ src/app/components/effective-visit/effective-visit.component.ts
@@ -15,12 +15,34 @@ export class EffectiveVisitComponent implements OnInit {
   @Input() effectiveVisitGoal: number = 0.65;
   // Recieve all the user indicators
   @Input() userIndicators: UserIndicators = new UserIndicators();
-
+  currentVisitPercentage: number = 0;
+  extraVisitsNeeded: number = 0;
   constructor() {}
 
   ngOnInit() {}
 
   ngOnChanges() {
+    this.calculateEffectiveVisitGoal();
+  }
+
+  /**
+   *Calculate the remaining effective visit to achieve the percentage goal
+   *
+   * @memberof EffectiveVisitComponent
+   */
+  calculateEffectiveVisitGoal() {
+    this.extraVisitsNeeded = 0;
+    if (
+      this.userIndicators.effectiveVisits / this.userIndicators.totalVisits <
+      this.effectiveVisitGoal
+    ) {
+      do {
+        this.extraVisitsNeeded++;
+        this.currentVisitPercentage =
+          (this.userIndicators.effectiveVisits + this.extraVisitsNeeded) /
+          (this.userIndicators.totalVisits + this.extraVisitsNeeded);
+      } while (this.currentVisitPercentage < this.effectiveVisitGoal);
+    }
     console.log(this.userIndicators);
   }
 }
